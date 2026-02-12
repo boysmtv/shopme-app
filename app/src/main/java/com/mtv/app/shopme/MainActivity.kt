fce31c8ba4521b095471c39e3133c986b25b9685
Dedy Wijaya <Boys.mtv@gmail.com>
2026-02-12 17:14:22 +0700

    chore: add home, detail, cart, search

--- app/src/main/java/com/mtv/app/shopme/MainActivity.kt
+++ app/src/main/java/com/mtv/app/shopme/MainActivity.kt
@@ -3,7 +3,6 @@ package com.mtv.app.shopme
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import com.mtv.app.shopme.nav.AppNavigation
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -11,7 +10,6 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             AppNavigation()
         }
