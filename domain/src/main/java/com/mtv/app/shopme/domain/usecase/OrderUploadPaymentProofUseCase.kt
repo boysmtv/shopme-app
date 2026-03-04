96acc956b3f3f2c5afbe7b9d654e0aea80e6c7f9
Dedy Wijaya <Boys.mtv@gmail.com>
2026-03-04 19:22:12 +0700

    chore: add usecase

---
+++ domain/src/main/java/com/mtv/app/shopme/domain/usecase/OrderUploadPaymentProofUseCase.kt
@@ -0,0 +1,12 @@
+/*
+ * Project: Shopme App
+ * Author: Boys.mtv@gmail.com
+ * File: OrderUploadPaymentProofUseCase.kt
+ *
+ * Last modified by Dedy Wijaya on 03/03/26 13.49
+ */
+
+package com.mtv.app.shopme.domain.usecase
+
+class OrderUploadPaymentProofUseCase {
+}
