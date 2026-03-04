848bcf7d80bcda65942453b14e15eca6597125d3
Dedy Wijaya <Boys.mtv@gmail.com>
2026-03-04 19:20:37 +0700

    chore: add usecase

---
+++ domain/src/main/java/com/mtv/app/shopme/domain/usecase/ChatMessageSendUseCase.kt
@@ -0,0 +1,12 @@
+/*
+ * Project: Shopme App
+ * Author: Boys.mtv@gmail.com
+ * File: ChatMessageSendUseCase.kt
+ *
+ * Last modified by Dedy Wijaya on 03/03/26 13.45
+ */
+
+package com.mtv.app.shopme.domain.usecase
+
+class ChatMessageSendUseCase {
+}
