4e18c04410f9880d8d3c62778b99c419478c5924
boysmtv <boys.mtv@gmail.com>
2026-06-01 22:36:15 +0700

    chore(build): increment versionCode to 9 for Firebase release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
         applicationId = "com.mtv.app.shopme"
         minSdk = 24
         targetSdk = 36
-        versionCode = 3
+        versionCode = 9
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
